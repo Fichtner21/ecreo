aa0b0906e9e381767a6713e4378c9e241ed4eb85
POZ-ERNESTF\e_fichtner <e.fichtner@sputnik.pl>
2019-05-30 23:06:30 +0200

    projekt 99%

--- js/main.js
+++ js/main.js
@@ -1,28 +1,20 @@
 $(document).ready(function(){
 
-	var parisFromList = $('.fra');
 	var list = $('.units__column');
-
-////
-	 var langSpan = $('.lang').find('span');
-	 var langDivs = $('.languages').find('div');
-	 console.log('lang span + ' + langSpan.length);
-	 console.log('langDivs + ' + langDivs.length);
-	
  
     var CircleIcon = L.Icon.extend({
     	options: {
     		//shadowUrl: 'images/circle2x.png',
 	    	iconSize: [10,10],
 	    	shadowSize: [20,20],
-	    	iconAnchor:   [-722, 610], // point of the icon which will correspond to marker's location
-	    	shadowAnchor: [-650, 605],  // the same for the shadow
+	    	iconAnchor:   [-722, 610], 
+	    	shadowAnchor: [-650, 605],  
 	    	popupAnchor:  [600, -540]
     	}    	
     });
 
     var ParisIcon = new CircleIcon({iconUrl: 'images/circle.png'}),
-    	OtherIcon = new CircleIcon({iconUrl: 'images/circle.png'}); //mozna inny png wrzucic
+    	OtherIcon = new CircleIcon({iconUrl: 'images/circle.png'}); 
 
     L.icon = function(options){
     	return new L.icon(options);
@@ -40,6 +32,12 @@ $(document).ready(function(){
     	'className' : 'otherMarker'
     }
 
+    var BauPopup = "<span class='up-line'>BAU</span><br/><div class='over'><div class='line'></div><div class='circle'></div></div><span class='below-line'>Bau(United States)</span>";
+    var BauOptions = {
+    	'maxWidth' : '300',
+    	'className' : 'bauMarker'
+    }
+
     var map = L.map('map', {
     	crs: L.CRS.Simple,
     	zoomControl: false, // usuniecie + - zoom
@@ -54,25 +52,13 @@ $(document).ready(function(){
     // var bounds = [[-26.5,-25], [1021.5,1023]];
     var image = L.imageOverlay('mapgood.png', bounds).addTo(map);
 
-    //var something = L.latLng([605,726],{title: 'BAU', icon:ParisIcon}).addTo(map).bindPopup(ParisPopup, ParisOptions);
-    //L.marker(paris).addTo(map);
-    // function addMarker(e){
-    // 	var newMarker = new L.marker([150,150]).addTo(map);
-    // }
-
-    // var bau = $('.bau');
-    // bau.on('click',function(){
-    // 	addMarker();
-    // })
-
-   
-    
-
     var markers = [];
     var ParisMarker = L.marker([0,0], {title: 'ParisMarker', icon:ParisIcon}).on('click', function(e){console.log(e.latlng)}).addTo(map).bindPopup(ParisPopup, ParisOptions);
     markers.push(ParisMarker);	
     var OtherMarker = L.marker([-100,600], {title: 'OtherMarker', icon:OtherIcon}).on('click', function(e){console.log(e.latlng)}).addTo(map).bindPopup(OtherPopup, OtherOptions);	
     markers.push(OtherMarker);
+    var BauMarker = L.marker([-50,-600], {title: 'BauMarker', icon:OtherIcon}).on('click', function(e){console.log(e.latlng)}).addTo(map).bindPopup(BauPopup, BauOptions);	
+    markers.push(BauMarker);
     map.setView([70,120], 1);
     map.fitBounds(bounds);
 
@@ -89,44 +75,16 @@ $(document).ready(function(){
   	
    	function markerFunction(id){
 	    for (var i in markers){
-	    	var icon = markers[i].options.icon;
-	  			//console.log("icon = " + JSON.stringify(icon));
+	    	var icon = markers[i].options.icon;	  			
 	    	var markerID = markers[i].options.title;
 	        if (markerID == id){
 	            markers[i].openPopup();
-	        };	
-
-	        
-	        //console.log('icon ' + icon);        	       
+	        };	            	       
 	    }
 	}
 
 	var markerHide = $('.leaflet-marker-icon');
-    markerHide.hide();
-
-	function compareTitle(c){
-		//for(var i in markers){
-			//console.log("markers: " + markers[i].options.title);
-			if($('.list').find('div').data('catch') == markers[i].options.title){
-				
-				console.log("to jest markers[i].options.title " + markers[i].options.title);
-				console.log("$('.list').find('div').data('catch') " + $('.list').find('div').data('catch'));
-				console.log('pasuje');
-				return true;
-			} else {
-				console.log('nie pasuje');
-				return false;
-			}
-		//}
-	}	
-
-	$('.list').children().each( function(){
-		$('list').find('div').on('click',function(){
-			compareTitle();
-		})
-	})
-
-	
+    markerHide.hide();	
 
 	$('.units__column').children().click(function(){
 		var markerTitle = $('.leaflet-marker-pane').find('img').attr('title');
@@ -143,20 +101,21 @@ $(document).ready(function(){
 	units.on('click', function(){
 		units.removeClass('highlight').addClass('lowlight');
 		$(this).addClass('highlight').removeClass('lowlight');
-		title.css({'visibility': 'hidden'});
-		
-	
-
+		//title.css({'visibility': 'hidden'});
+		title.parent().addClass('hid');
 		popup.on('click',function(){
 			units.addClass('highlight');
 		});
 	});	
-		
-		$(document).on('click', function(e){			
-		if(title.css('visibility') === 'hidden'){
-			//title.css({'visibility':'visible'});
+
+	$('.site__logo').on('click', function(){		
+		if($('.title').hasClass('hid')){
+			$('.title').addClass('vis').removeClass('hid');
+
+			console.log('ma klase hid');
 		}
-	});
+		units.removeClass('lowlight').addClass('highlight');
+	});		
    
 	map.dragging.disable();
 	map.touchZoom.disable();
@@ -165,15 +124,6 @@ $(document).ready(function(){
 	map.boxZoom.disable();
 	map.keyboard.disable();
 	if (map.tap) map.tap.disable();
-	document.getElementById('map').style.cursor='default';    
-
-
-	var caret = $('.fa-chevron-down');
-	var langs = $('.langauges div');
-	console.log(langs);
-
-	caret.on('click', function(){
-		console.log('kilk');
-		langs.css({'visibility':'visible'});
-	});
+	document.getElementById('map').style.cursor='default'; 
+	
 });
